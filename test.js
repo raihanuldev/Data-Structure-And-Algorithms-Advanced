a16fb8925d9eaece29251ab502a1df1e8931e7e4
raihanuldev <rihanthedev@gmail.com>
2024-06-14 02:39:50 +0600

    function

--- test.js
+++ test.js
@@ -5,14 +5,30 @@ const userAges = [12,13,34,563,564]
 const professions = ["doctor","programer","eng","rickshawala"]
 const user = {name: users[1],age: userAges[1], profession: professions[1] }
 
-for (let i =0; i < users.length; i ++){
-    // const element = users[i]
-    // console.log(i);
-    // console.log("Current Users Name: ", element);
-    // console.log("Current Users Age: ", userAges[i]);
-    const newUserInfo = {name: users[i],age: userAges[i], job: professions[i]}
-    console.log(newUserInfo);
+
+
+// Practice some concepts
+
+function sum(a,b){
+    const sum = a+b
+    return sum;
 }
+console.log(sum(12,23));
+
+// es6
+const multiply = (a,b)=>{
+    return a*b
+}
+console.log(multiply(12,2));
+
+// for (let i =0; i < users.length; i ++){
+//     // const element = users[i]
+//     // console.log(i);
+//     // console.log("Current Users Name: ", element);
+//     // console.log("Current Users Age: ", userAges[i]);
+//     const newUserInfo = {name: users[i],age: userAges[i], job: professions[i]}
+//     console.log(newUserInfo);
+// }
 
 
 
