63e92d5eea9917be2a22d23ad8ac352f4a49084a
raihanuldev <rihanthedev@gmail.com>
2024-06-14 02:31:06 +0600

    code test

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log("Hello world Iam Here");
+
+const users = ["raihan","mahin","habib"]
+const userAges = [12,13,34,563,564]
+const professions = ["doctor","programer","eng","rickshawala"]
+const user = {name: users[1],age: userAges[1], profession: professions[1] }
+
+console.log(user);
