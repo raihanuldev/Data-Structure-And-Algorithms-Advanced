1e89bcda1bfcf2e7233cd44b60ab49fdfaa83e71
raihanuldev <rihanthedev@gmail.com>
2024-06-14 02:36:37 +0600

    for loop

--- test.js
+++ test.js
@@ -5,4 +5,15 @@ const userAges = [12,13,34,563,564]
 const professions = ["doctor","programer","eng","rickshawala"]
 const user = {name: users[1],age: userAges[1], profession: professions[1] }
 
-console.log(user);
+for (let i =0; i < users.length; i ++){
+    // const element = users[i]
+    // console.log(i);
+    // console.log("Current Users Name: ", element);
+    // console.log("Current Users Age: ", userAges[i]);
+    const newUserInfo = {name: users[i],age: userAges[i], job: professions[i]}
+    console.log(newUserInfo);
+}
+
+
+
+// console.log(user);
