da4e3dc64b8d837f591de46597d7dfd97779469b
raihanuldev <rihanthedev@gmail.com>
2024-06-15 03:28:14 +0600

    big O (n)

---
+++ bigONotation/bigOnotationCalculete.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 
+ * */ 
+
+function min5Log (n){
+    for(let i =0; i <= Math.max(5,n); i++){
+        console.log(i);
+    }
+}
+
+min5Log(5) //big o (n) , becuase eikhane purata n er upor defend Kortese
+
+// lets go another function
