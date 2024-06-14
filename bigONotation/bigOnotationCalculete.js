fd369eabd8f7e1d66163b1da00cdc2011139c527
raihanuldev <rihanthedev@gmail.com>
2024-06-15 03:35:48 +0600

    time complexity calculete

--- bigONotation/bigOnotationCalculete.js
+++ bigONotation/bigOnotationCalculete.js
@@ -12,3 +12,10 @@ function min5Log (n){
 min5Log(5) //big o (n) , becuase eikhane purata n er upor defend Kortese
 
 // lets go another function
+
+function max5Log(n){
+    for(let i=0; i<= Math.min(5,n); i++){
+        console.log(i);
+    }
+}
+max5Log(2) // big o notation hocce o(1)
