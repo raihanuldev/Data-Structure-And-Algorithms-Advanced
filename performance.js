6fc487afe59f9d97ef237b40fae9aef8c51da368
raihanuldev <rihanthedev@gmail.com>
2024-06-14 12:11:51 +0600

    code scaleable

--- performance.js
+++ performance.js
@@ -1,3 +1,4 @@
+// low code for this function. 
 function sumOFall(n){
     let total = 0;
     for(let i = 0; i<=n; i++){
@@ -5,6 +6,11 @@ function sumOFall(n){
         
     }
     return total
+}
+
+// Good Code 
+function sumOFall(n){
+    return n*(n+1)/2
 }
 
 console.log(sumOFall(5));
