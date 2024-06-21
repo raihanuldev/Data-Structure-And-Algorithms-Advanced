a4bb13a0b5acc780e25ab579bc2cfacdebe8532c
raihanuldev <rihanthedev@gmail.com>
2024-06-21 14:35:17 +0600

    problem solveing apporce

---
+++ problemSolveingApproce.js
@@ -0,0 +1,27 @@
+/**
+ * Problem: find higest temp ,lowest temp, find any error,
+ */
+let temp = [-2, 45, 23, 41, "error", 65, 14, 53];
+
+function getHigherAndLower(arr) {
+  // Create a Higher variable
+  let higher = arr[0];
+  // createa lower variable
+  let lower = arr[0];
+  // iterate the whole array
+  for (let i = 0; i < arr.length; i++) {
+    // lets check is there has any error
+    // if higer variable has small number than our current number than value will be our current element
+    if (higher < arr[i]) {
+        higher = arr[i]
+    }
+    // if lower variable has bigger number than our current number than value will be our current element
+    if(lower > arr[i]){
+        lower = arr[i]
+    }
+  }
+  console.log(higher,lower);
+  // return highest value and lowest value
+}
+
+getHigherAndLower([2, 3, 4, 6]);
