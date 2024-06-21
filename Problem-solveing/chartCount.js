5a5f62deea9568d2362170b74d59ae3f15b2d672
raihanuldev <rihanthedev@gmail.com>
2024-06-22 01:33:41 +0600

    problem-solveing charCoutn

---
+++ Problem-solveing/chartCount.js
@@ -0,0 +1,20 @@
+// find charactare from string
+
+function charCount(str) {
+  // charList declere
+  let charList = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i]
+    if(charList[char] >0){
+        charList[char]++
+    }
+    else{
+        charList[char] =1
+    }
+    // console.log(charList[char]);
+  }
+  console.log(charList);
+}
+
+charCount("hello Everyone");
