8f61de802ef3bd6291619b260ce27c9df47230ec
raihanuldev <rihanthedev@gmail.com>
2024-06-21 14:41:13 +0600

    problem 01

--- problemSolveingApproce.js
+++ problemSolveingApproce.js
@@ -10,18 +10,19 @@ function getHigherAndLower(arr) {
   let lower = arr[0];
   // iterate the whole array
   for (let i = 0; i < arr.length; i++) {
-    // lets check is there has any error
+    // lets check is there has any error if error than ignore
+    if (typeof arr[i] !== "number") continue;
     // if higer variable has small number than our current number than value will be our current element
     if (higher < arr[i]) {
-        higher = arr[i]
+      higher = arr[i];
     }
     // if lower variable has bigger number than our current number than value will be our current element
-    if(lower > arr[i]){
-        lower = arr[i]
+    if (lower > arr[i]) {
+      lower = arr[i];
     }
   }
-  console.log(higher,lower);
-  // return highest value and lowest value
+  console.log(higher, lower);
+  return higher -lower
 }
 
-getHigherAndLower([2, 3, 4, 6]);
+getHigherAndLower(temp);
