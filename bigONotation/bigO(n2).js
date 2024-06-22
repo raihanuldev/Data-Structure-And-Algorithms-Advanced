7753bbcd8a468f77fe17609ac1bd470364405f7a
raihanuldev <rihanthedev@gmail.com>
2024-06-23 03:21:06 +0600

    Quadratic Time Complexity

--- bigONotation/bigO(n2).js
+++ bigONotation/bigO(n2).js
@@ -1,4 +1,9 @@
 
+/*
+Quadratic Time Complexity: Algorithms with O(n^2) complexity have a runtime that grows quadratically with the input size. 
+This means that as the input size increases, the runtime of the algorithm increases exponentially. 
+An example of O(n^2) complexity is nested loops where each loop iterates over the input.
+*/ 
 // n square big notation example 
 function multiply (n){
     for(let i =0; i <=n; i++){ // n complexity
