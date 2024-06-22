e44988cbd4f8760575855c1a5e99dd1e1aa58902
raihanuldev <rihanthedev@gmail.com>
2024-06-23 02:58:31 +0600

    bigO(n) & bigO(n)2 complexity

---
+++ bigONotation/bigO(n).js
@@ -0,0 +1,8 @@
+
+function multiply (n){
+    for(let i =0; i <=n; i++){ // n complexity
+        
+    }
+}
+// This function Big O(n)
+multiply(2)
---
+++ bigONotation/bigO(n2).js
@@ -0,0 +1,11 @@
+
+// n square big notation example 
+function multiply (n){
+    for(let i =0; i <=n; i++){ // n complexity
+        for(let x =0; x<=n; x++){ // n complexity
+            console.log(i,x);
+        }
+    }
+}
+// This function Big O(n2)
+multiply(2)
