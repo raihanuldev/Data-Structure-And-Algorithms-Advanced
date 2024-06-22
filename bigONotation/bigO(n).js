586ff23a16bf1556cdbe1814a0a65345871e9c0c
raihanuldev <rihanthedev@gmail.com>
2024-06-23 03:19:38 +0600

    Liner Time Complexity O(n)

--- bigONotation/bigO(n).js
+++ bigONotation/bigO(n).js
@@ -1,7 +1,11 @@
-
+/*
+O(n) - Linear Time Complexity: Algorithms with O(n) complexity have a runtime that grows linearly with the input size.
+This means that as the input size increases, the runtime of the algorithm increases proportionally. 
+An example of O(n) complexity is iterating through an array and performing a specific operation on each element.
+*/ 
 function multiply (n){
     for(let i =0; i <=n; i++){ // n complexity
-        
+        console.log(n);
     }
 }
 // This function Big O(n)
