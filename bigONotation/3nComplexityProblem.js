198c03a37a3903e88cb0d3777f273ccd9a883c32
raihanuldev <rihanthedev@gmail.com>
2024-06-24 09:42:14 +0600

    3n complexity

--- bigONotation/3nComplexityProblem.js
+++ bigONotation/3nComplexityProblem.js
@@ -1,28 +1,5 @@
-// // we have an 2 array. if match all fristArray square value with secound array then return true. otherWays false.
-// // if elements not same then retun false
+// 3n complexity
 
-// function same(arr1,arr2){
-//     // Frist Step 
-//     if(arr1.length !== arr2.length){ 
-//         return false
-//     }
-    
-//     for(let i =0; i<arr1.length; i++){
-//         let currentIndex = arr2.indexOf(arr1[i] **2)
-//         if(currentIndex === -1){
-//             return false
-//         }
-//         arr2.splice(currentIndex,1)
-//     }
-//     return true
-
-// }
-// // n*n => n2 time complexity
-// // that's huge .
-
-
-
-// better complexity
 function same(arr1,arr2){
     if(arr1.length !== arr2.length){
         return false;
@@ -54,3 +31,5 @@ function same(arr1,arr2){
 }
 
 console.log(same([2,3,6],[9,4,36]));   //should be return of true
+
+//total complexity => n+n+n => 3n 
