b3d38d8ad77aca6a6d62d5140f9bee10f6aba74e
raihanuldev <rihanthedev@gmail.com>
2024-06-24 10:35:33 +0600

    recusrsion

--- recursion/sumOfNumber.js
+++ recursion/sumOfNumber.js
@@ -1,10 +1,17 @@
 
+// function sumOfNumber(n){
+//     let total = 0;
+//     for(let i = 1; i <= n; i++){
+//         total = total+i
+//     }
+//     console.log(total);
+// }
+
+
+// sum of with Recursion way
 function sumOfNumber(n){
-    let total = 0;
-    for(let i = 1; i <= n; i++){
-        total = total+i
-    }
-    console.log(total);
+    if(n<0) return 0;
+    return n + sumOfNumber(n-1)
 }
 
-sumOfNumber(10)
+console.log(sumOfNumber(10));
