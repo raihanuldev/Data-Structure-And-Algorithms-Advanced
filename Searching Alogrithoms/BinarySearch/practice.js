a1c1d43c9c1da0be676529ef8ed528bf23470233
raihanuldev <rihanthedev@gmail.com>
2024-06-25 17:15:35 +0600

    BinarySearch

---
+++ Searching Alogrithoms/BinarySearch/practice.js
@@ -0,0 +1,21 @@
+function binarySearch(arr,value){
+    // you neeed to 3 value like start,middle,end
+    let start = 0;
+    let end = arr.length -1
+
+    while(start <= end){
+        let middle = Math.floor(start +end) /2
+
+        if(arr[middle] === value) return middle;
+
+        if(value > arr[middle]){
+            start = start+1
+        } 
+        else {
+            end = end -1
+        }
+    }
+    return -1
+
+}
+console.log(binarySearch([1,2,3,4,5,6],4));
