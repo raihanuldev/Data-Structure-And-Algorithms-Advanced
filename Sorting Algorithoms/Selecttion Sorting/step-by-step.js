23c69b796dc95143a0fbd56c9676ea8ec7468b64
raihanuldev <rihanthedev@gmail.com>
2024-06-27 03:36:37 +0600

    create an doc fo slection sort

---
+++ Sorting Algorithoms/Selecttion Sorting/step-by-step.js
@@ -0,0 +1,14 @@
+/**
+ * Create an function
+ * in this function useing for loop
+ * ont this for loop create an variable which is hold lowest value
+ * then create an nested loop (let j = i+1)
+ * check if array[lowest] > arr[j] then set   lowest value => j (index not value)
+ * then out of nested loop create an temp variable for swap
+ * now swap 
+ * // SWAP
+        let temp = arr[i];
+        arr[i] = arr[lowest]
+        arr[lowest] = temp
+ * retun arr =>
+*/
