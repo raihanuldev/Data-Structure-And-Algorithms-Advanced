ca909e19fd07b699a616ad6c27669c88cc3d736b
raihanuldev <rihanthedev@gmail.com>
2024-06-27 04:06:59 +0600

    insert Sort

---
+++ Sorting Algorithoms/insert Sorting/insertSort.js
@@ -0,0 +1,15 @@
+function insertSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for(let j = i; j>0; j--){
+        // console.log( i,j);
+        if(arr[j] < arr[j-1]){
+            let temp = arr[j];
+            arr[j] = arr[j-1]
+            arr[j-1] = temp;
+        }
+    }
+  }
+  return arr;
+}
+
+console.log(insertSort([-12, 3, 1, 5, 9, 7]));
