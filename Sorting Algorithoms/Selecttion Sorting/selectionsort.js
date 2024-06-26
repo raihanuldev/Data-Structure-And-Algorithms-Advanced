bc429df56a129580dde1b4e5ce70f0b0e05f8476
raihanuldev <rihanthedev@gmail.com>
2024-06-27 03:30:43 +0600

    Selection sorting

---
+++ Sorting Algorithoms/Selecttion Sorting/selectionsort.js
@@ -0,0 +1,20 @@
+
+
+function selectionSort(arr){
+    for(let i = 0; i<arr.length; i++){
+        let lowest = i
+        for(let j = i+1; j<arr.length; j++){
+            // console.log(i,j);
+            if(arr[lowest] > arr[j]){
+                lowest = j
+            }
+        }
+        // SWAP
+        let temp = arr[i];
+        arr[i] = arr[lowest]
+        arr[lowest] = temp
+    }
+    return arr
+}
+
+console.log(selectionSort([23,12,-6,3,8]));
