99b7f1e707aa249d328b218a3380c9f1e7cec67d
raihanuldev <rihanthedev@gmail.com>
2024-06-27 03:43:16 +0600

    Best Optimized Code

--- Sorting Algorithoms/Selecttion Sorting/selectionsort.js
+++ Sorting Algorithoms/Selecttion Sorting/selectionsort.js
@@ -1,20 +1,21 @@
-
-
-function selectionSort(arr){
-    for(let i = 0; i<arr.length; i++){
-        let lowest = i
-        for(let j = i+1; j<arr.length; j++){
-            // console.log(i,j);
-            if(arr[lowest] > arr[j]){
-                lowest = j
-            }
-        }
-        // SWAP
-        let temp = arr[i];
-        arr[i] = arr[lowest]
-        arr[lowest] = temp
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      // console.log(i,j);
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
     }
-    return arr
+    if (i !== lowest) {
+      // SWAP
+      console.log(arr, i, lowest);
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
 }
 
-console.log(selectionSort([23,12,-6,3,8]));
+console.log(selectionSort([23, 12, -6, 3, 8]));
