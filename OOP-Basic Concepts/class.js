900a01c35283619782b3da16275c60b36b45faeb
raihanuldev <rihanthedev@gmail.com>
2024-06-29 04:24:25 +0600

    Method (insert)

--- OOP-Basic Concepts/class.js
+++ OOP-Basic Concepts/class.js
@@ -4,7 +4,11 @@ class DataStructure {
         this.name = name,
         this.topic = [topic]
     }
+    insert(value){
+        this.topic.push(value)
+        return this.topic
+    }
 }
 
 let ds =new DataStructure("Link-List","DSA Fundamentals")
-console.log(ds);
+console.log(ds.insert("Be a-Great Problem Solver"));
