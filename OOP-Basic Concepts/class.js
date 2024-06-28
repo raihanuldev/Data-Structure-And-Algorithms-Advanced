6daeb30052c75a097733d225dee1b251784a54e9
raihanuldev <rihanthedev@gmail.com>
2024-06-29 04:22:12 +0600

    class

---
+++ OOP-Basic Concepts/class.js
@@ -0,0 +1,10 @@
+
+class DataStructure {
+    constructor(name,topic,){
+        this.name = name,
+        this.topic = [topic]
+    }
+}
+
+let ds =new DataStructure("Link-List","DSA Fundamentals")
+console.log(ds);
