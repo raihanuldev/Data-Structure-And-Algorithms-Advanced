75923d25ff16a281e05a8cc2547fa74d5dd75f03
raihanuldev <rihanthedev@gmail.com>
2024-06-30 04:30:30 +0600

    singly Linked List

--- LinkedList/singlyLinkedList.js
+++ LinkedList/singlyLinkedList.js
@@ -9,6 +9,29 @@ class SinglyLinkedList{
     isEmpty(){
         return this.length ===0;
     }
+
+    // Push new Node
+     push(value){
+        let newNode = {
+            value: value,
+            next: null
+        }
+        if(this.isEmpty()){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode
+        }
+        this.length++
+     }
 }
 let list = new SinglyLinkedList();
-console.log(list.isEmpty());
+
+console.log(list);
+list.push(5)
+console.log(list);
+list.push(10)
+console.log(list);
+list.push(15)
+console.log(list.head.next);
