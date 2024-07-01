8818e5b94610b1a9eca94ea1b59fe15f8f66b637
raihanuldev <rihanthedev@gmail.com>
2024-07-02 05:49:46 +0600

    pop

--- LinkedList/singlyLinkedList.js
+++ LinkedList/singlyLinkedList.js
@@ -25,13 +25,37 @@ class SinglyLinkedList{
         }
         this.length++
      }
+    // Pop
+    pop(){
+        if(!this.head){
+            return null;
+        }
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null
+        }
+
+        let currentNode = this.head;
+        let lastNode = this.tail;
+        let newLastNode;
+
+        while(currentNode){
+            if(currentNode.next == this.tail){
+                newLastNode = currentNode;
+                break;
+            }
+            currentNode = currentNode.next;
+        }
+        newLastNode.next = null;
+        this.tail = newLastNode;
+        this.length--
+        return lastNode;
+    }
 }
 let list = new SinglyLinkedList();
 
-console.log(list);
 list.push(5)
-console.log(list);
 list.push(10)
-console.log(list);
 list.push(15)
-console.log(list.head.next);
+list.pop()
+console.log(list);
