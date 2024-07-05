80c570a7c194124393016e024bed47a8b3791869
raihanuldev <rihanthedev@gmail.com>
2024-07-05 06:32:32 +0600

    push

--- LinkedList/doubleyLinkList.js
+++ LinkedList/doubleyLinkList.js
@@ -12,6 +12,23 @@ class DoublelyLinkList{
         this.tail = null;
         this.length = 0;
     }
+    // push
+    push(value){
+        let newNode = new Node(value);
+
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
 }
 let list = new DoublelyLinkList()
+list.push(10)
+list.push(12)
+list.push(13)
 console.log(list);
