989418ef779435958219aaa3127b28f08f848ded
raihanuldev <rihanthedev@gmail.com>
2024-07-06 01:52:55 +0600

    frist node added

---
+++ StackWithLinkList/stackwithLinkList.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+  }
+}
+class BookStack {
+    constructor(){
+        this.top = null;
+        this.bottom = null;
+        this.length = 0
+    }
+    push(value){
+        let book = new Node(value);
+        if(!this.top){
+            this.top = book;
+            this.bottom = book;
+            this.length++
+        }
+    }
+}
+let bookStacks = new BookStack();
+bookStacks.push("Book-1")
+console.log(bookStacks);
