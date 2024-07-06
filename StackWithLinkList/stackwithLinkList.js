1b6d75da7b1341071430383895408db942680a46
raihanuldev <rihanthedev@gmail.com>
2024-07-06 08:34:43 +0600

    stackwithLinkList

--- StackWithLinkList/stackwithLinkList.js
+++ StackWithLinkList/stackwithLinkList.js
@@ -4,22 +4,27 @@ class Node {
   }
 }
 class BookStack {
-    constructor(){
-        this.top = null;
-        this.bottom = null;
-        this.length = 0
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  push(value) {
+    let book = new Node(value);
+    if (!this.top) {
+      this.top = book;
+      this.bottom = book;
+    } else {
+      let holdingBooks = this.top;
+      this.top = book;
+      this.top.next = holdingBooks;
     }
-    push(value){
-        let book = new Node(value);
-        if(!this.top){
-            this.top = book;
-            this.bottom = book;
-            this.length++
-        }else{
-            
-        }
-    }
+    
+    this.length++;
+  }
 }
 let bookStacks = new BookStack();
-bookStacks.push("Book-1")
+bookStacks.push("Book-1");
+bookStacks.push("Book-2");
+bookStacks.push("Book-3");
 console.log(bookStacks);
